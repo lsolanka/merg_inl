fd966f7fed6c4afae85dcac6dbfbb5820691bbaf
Lukas Solanka <lukas.solanka@sevensense.ch>
2023-11-02 23:04:18 +0100

    implement function to get parent file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 pub fn get_parent_file_path(inl_file: &str) -> Option<PathBuf> {
-    if inl_file.ends_with("-inl.h") {
+    if inl_file.ends_with("-inl.h") || inl_file.ends_with("_inl.h") {
         let parent = String::from(&inl_file[0..inl_file.len()-6]) + ".h";
         Some(PathBuf::from(parent))
     } else {
@@ -11,8 +11,37 @@ pub fn get_parent_file_path(inl_file: &str) -> Option<PathBuf> {
 
 #[cfg(test)]
 mod test {
-    #[test]
-    fn test_get_parent_file_path() {
+mod test_get_parent_file_path {
+    use crate::get_parent_file_path;
+    use std::path::PathBuf;
 
+    #[test]
+    fn empty_input() {
+        let parent_path = get_parent_file_path("");
+        assert!(parent_path.is_none());
     }
+
+    #[test]
+    fn correct_input_with_dash() {
+        let parent_path = get_parent_file_path("dir/fancy-inl.h");
+        assert!(parent_path.is_some());
+        assert_eq!(parent_path.unwrap(), PathBuf::from("dir/fancy.h"));
+    }
+
+    #[test]
+    fn correct_input_with_underscore() {
+        let parent_path = get_parent_file_path("dir/fancy_inl.h");
+        assert!(parent_path.is_some());
+        assert_eq!(parent_path.unwrap(), PathBuf::from("dir/fancy.h"));
+    }
+
+    #[test]
+    fn no_inl_in_input() {
+        let parent_path = get_parent_file_path("dir/");
+        assert!(parent_path.is_none());
+
+        let parent_path = get_parent_file_path("dir/fancy.h");
+        assert!(parent_path.is_none());
+    }
+}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use merge_inl;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,6 +11,10 @@ struct Args {
 fn main() {
     let args = Args::parse();
     for arg in args.files.iter() {
-        println!("{}", arg);
+        if let Some(parent_path) = merge_inl::get_parent_file_path(&arg) {
+            println!("{} parent: {}", arg, parent_path.to_str().unwrap());
+        } else {
+            eprintln!("{} is not a file with `-inl.h` suffix; skipping", arg);
+        }
     }
 }
