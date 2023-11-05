1e71b956b23280388340855e9f6870b03ee5051d
Lukas Solanka <lukas.solanka@sevensense.ch>
2023-11-05 22:36:04 +0100

    add input file readability checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-pub fn get_parent_file_path(inl_file: &str) -> Option<PathBuf> {
-    if inl_file.ends_with("-inl.h") || inl_file.ends_with("_inl.h") {
-        let parent = String::from(&inl_file[0..inl_file.len() - 6]) + ".h";
+pub fn get_parent_file_path(inl_file: &Path) -> Option<PathBuf> {
+    if inl_file.to_string_lossy().ends_with("-inl.h")
+        || inl_file.to_string_lossy().ends_with("_inl.h")
+    {
+        let file_as_str = inl_file.to_string_lossy();
+        let parent = String::from(&file_as_str[0..file_as_str.len() - 6]) + ".h";
         Some(PathBuf::from(parent))
     } else {
         None
@@ -18,30 +21,30 @@ mod test {
 
         #[test]
         fn empty_input() {
-            let parent_path = get_parent_file_path("");
+            let parent_path = get_parent_file_path(&PathBuf::from(""));
             assert!(parent_path.is_none());
         }
 
         #[test]
         fn correct_input_with_dash() {
-            let parent_path = get_parent_file_path("dir/fancy-inl.h");
+            let parent_path = get_parent_file_path(&PathBuf::from("dir/fancy-inl.h"));
             assert!(parent_path.is_some());
             assert_eq!(parent_path.unwrap(), PathBuf::from("dir/fancy.h"));
         }
 
         #[test]
         fn correct_input_with_underscore() {
-            let parent_path = get_parent_file_path("dir/fancy_inl.h");
+            let parent_path = get_parent_file_path(&PathBuf::from("dir/fancy_inl.h"));
             assert!(parent_path.is_some());
             assert_eq!(parent_path.unwrap(), PathBuf::from("dir/fancy.h"));
         }
 
         #[test]
         fn no_inl_in_input() {
-            let parent_path = get_parent_file_path("dir/");
+            let parent_path = get_parent_file_path(&PathBuf::from("dir/"));
             assert!(parent_path.is_none());
 
-            let parent_path = get_parent_file_path("dir/fancy.h");
+            let parent_path = get_parent_file_path(&PathBuf::from("dir/fancy.h"));
             assert!(parent_path.is_none());
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,74 @@
+use std::error::Error;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::process;
+
 use clap::Parser;
+use faccess::PathExt;
 use merge_inl;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// List of -inl.h files to merge into their parents
-    files: Vec<String>,
+    files: Vec<PathBuf>,
 }
 
-fn main() {
-    let args = Args::parse();
-    for arg in args.files.iter() {
-        if let Some(parent_path) = merge_inl::get_parent_file_path(&arg) {
-            println!("{} parent: {}", arg, parent_path.to_str().unwrap());
-        } else {
-            eprintln!("{} is not a file with `-inl.h` suffix; skipping", arg);
+fn check_if_args_exist(file_paths: &Vec<PathBuf>) -> Result<(), io::Error> {
+    let mut bad_files: Vec<&Path> = vec![];
+
+    for file_path in file_paths.iter() {
+        if !(file_path.is_file() && file_path.readable()) {
+            bad_files.push(file_path);
         }
     }
+
+    if !bad_files.is_empty() {
+        return Err(io::Error::new(
+            io::ErrorKind::PermissionDenied,
+            String::from("The following files are not readable:\n")
+                + &file_list_to_string(&bad_files),
+        ));
+    }
+
+    Ok(())
+}
+
+fn file_list_to_string(files: &Vec<&Path>) -> String {
+    let mut file_string = String::new();
+
+    for file in files.iter() {
+        file_string += &format!("{}\n", file.to_string_lossy());
+    }
+
+    file_string
+}
+
+pub fn merge_inl_files(inl_files: &Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+    if let Err(error) = check_if_args_exist(inl_files) {
+        eprint!("{}", error.to_string());
+        process::exit(1);
+    }
+
+    for inl_file in inl_files.iter() {
+        if let Some(parent_path) = merge_inl::get_parent_file_path(inl_file) {
+            println!(
+                "{} parent: {}",
+                inl_file.display(),
+                parent_path.to_str().unwrap()
+            );
+        } else {
+            eprintln!(
+                "{} is not a file with `-inl.h` or `_inl.h` suffix; skipping",
+                inl_file.display()
+            );
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    merge_inl_files(&args.files)
 }
